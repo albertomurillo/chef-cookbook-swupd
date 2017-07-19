90c307a25b1ad9c43a5cd128f288238995d6f77a
Alberto Murillo Silva <alberto.murillo.silva@intel.com>
2017-07-19 10:56:52 -0500

    Update Readme and Metadata
    Signed-off-by: Alberto Murillo Silva <alberto.murillo.silva@intel.com>

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'swupd'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Alberto Murillo'
+maintainer_email 'alberto.murillo.silva@intel.com'
 license 'All Rights Reserved'
-description 'Installs/Configures swupd'
-long_description 'Installs/Configures swupd'
+description 'Extends package resource with swupd provider for ClearLinux nodes'
+long_description 'Extends package resource with swupd provider for ClearLinux nodes'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
@@ -11,10 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/swupd/issues'
+issues_url 'https://github.com/albertomurillo/chef-cookbook-swupd/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/swupd'
+source_url 'https://github.com/albertomurillo/chef-cookbook-swupd'
