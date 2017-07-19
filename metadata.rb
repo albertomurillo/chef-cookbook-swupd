6edd31abbf40164028d64edfde3ec961889f2984
Alberto Murillo Silva <alberto.murillo.silva@intel.com>
2017-07-19 11:23:16 -0500

    Add supported platforms
    Signed-off-by: Alberto Murillo Silva <alberto.murillo.silva@intel.com>

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description 'Extends package resource with swupd provider for ClearLinux nodes'
 long_description 'Extends package resource with swupd provider for ClearLinux nodes'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'clearlinux'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
